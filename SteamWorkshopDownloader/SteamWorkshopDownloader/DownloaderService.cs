da5727d76ea80bf8c4a4a8582c0a49f3f3568abc
LaoSparrow <laosparrow@outlook.com>
2026-03-06 00:37:53 +0800

    feat: basic project framework

---
+++ SteamWorkshopDownloader/SteamWorkshopDownloader/DownloaderService.cs
@@ -0,0 +1,54 @@
+using System.Collections.Concurrent;
+using System.Web;
+using GenHTTP.Api.Protocol;
+using GenHTTP.Modules.Reflection;
+using GenHTTP.Modules.Webservices;
+
+namespace SteamWorkshopDownloader;
+
+public class DownloaderService
+{
+    public static ConcurrentDictionary<ulong, DateTime> LastDownloads { get; } = new();
+    public static readonly TimeSpan Interval = TimeSpan.FromHours(1);
+    
+    public record PostQueueRequest(string UrlOrId);
+    public record PostQueueResponse(bool IsSuccess, string Message);
+    
+    [ResourceMethod(RequestMethod.Post, "queue")]
+    public PostQueueResponse PostQueue([FromBody] PostQueueRequest request)
+    {
+        var extractedPubFileId = ulong.MaxValue;
+        if (Uri.TryCreate(request.UrlOrId, UriKind.Absolute, out var uri))
+        {
+            if (uri.Host != "steamcommunity.com")
+                return new PostQueueResponse(false, "not a url to steamcommunity.com");
+            if (uri.Scheme != "http" && uri.Scheme != "https")
+                return new PostQueueResponse(false, "not a url to steamcommunity.com");
+            
+            var queryParams = HttpUtility.ParseQueryString(uri.Query);
+            var mightBeId = queryParams["id"];
+            if (string.IsNullOrEmpty(mightBeId))
+                return new PostQueueResponse(false, "id param is not a id");
+            if (!ulong.TryParse(mightBeId, out extractedPubFileId))
+                return new PostQueueResponse(false, "id param is not a id");
+        }
+        if (ulong.TryParse(request.UrlOrId, out var pubFileId))
+        {
+            extractedPubFileId = pubFileId;
+        }
+        if (extractedPubFileId == ulong.MaxValue)
+        {
+            return new PostQueueResponse(false, "not a valid URL or ID");
+        }
+
+        if (LastDownloads.TryGetValue(extractedPubFileId, out var lastDownload) && DateTime.UtcNow - lastDownload < Interval)
+        {
+            return new PostQueueResponse(false, "already downloaded within an hour");
+        }
+        LastDownloads[extractedPubFileId] = DateTime.UtcNow;
+        var result = WorkshopDownloader.TryPushDownloadQueue(extractedPubFileId);
+        return result
+            ? new PostQueueResponse(true, "Successfully posted queue!")
+            : new PostQueueResponse(false, "Failed to post queue! Too many queued items");
+    }
+}
---
+++ SteamWorkshopDownloader/SteamWorkshopDownloader/WorkshopDownloader.cs
@@ -0,0 +1,128 @@
+using System.Threading.Channels;
+using DepotDownloader;
+
+namespace SteamWorkshopDownloader;
+
+public static class WorkshopDownloader
+{
+    public record DownloadQueueEntry(ulong PubFileId, DateTime RequestDateTime);
+    
+    public static Task? DownloaderTask;
+    public static CancellationTokenSource Cts = new();
+    public static Channel<DownloadQueueEntry> DownloadQueue = Channel.CreateBounded<DownloadQueueEntry>(5);
+    public static Channel<(DownloadQueueEntry?, Exception)> ExceptionQueue = Channel.CreateUnbounded<(DownloadQueueEntry?, Exception)>();
+    public static Channel<(DownloadQueueEntry?, string)> MessageQueue = Channel.CreateUnbounded<(DownloadQueueEntry?, string)>();
+
+    public static DownloadQueueEntry? CurrentDownloadEntry;
+
+    public const uint APP_ID = 1281930;
+
+    public static void Init()
+    {
+        Cts.TryReset();
+        DownloadQueue = Channel.CreateBounded<DownloadQueueEntry>(5);
+        DownloaderTask = Task.Run(DownloaderLoop, Cts.Token);
+
+        ContentDownloader.Config.MaxDownloads = 8;
+        AccountSettingsStore.LoadFromFile("account.config");
+        
+        // TODO: true handling
+        Task.Run(async () =>
+        {
+            while (!Cts.IsCancellationRequested)
+            {
+                await foreach (var i in ExceptionQueue.Reader.ReadAllAsync(Cts.Token))
+                {
+                    Console.WriteLine(i.Item1);
+                    Console.WriteLine(i.Item2);
+                }
+            }
+        });
+        Task.Run(async () =>
+        {
+            while (!Cts.IsCancellationRequested)
+            {
+                await foreach (var i in MessageQueue.Reader.ReadAllAsync(Cts.Token))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        });
+    }
+
+    public static async Task Shutdown()
+    {
+        await Cts.CancelAsync();
+        DownloadQueue.Writer.Complete();
+        if (DownloaderTask != null)
+        {
+            await DownloaderTask.WaitAsync(CancellationToken.None);
+        }
+        DownloaderTask = null;
+    }
+
+    public static bool TryPushDownloadQueue(ulong pubFileId)
+    {
+        return DownloadQueue.Writer.TryWrite(new DownloadQueueEntry(pubFileId, DateTime.UtcNow));
+    } 
+
+    private static async Task DownloaderLoop()
+    {
+        Action<string> msgCallback = msg => MessageQueue.Writer.TryWrite((CurrentDownloadEntry, msg));
+        ContentDownloader.MessageCallback += msgCallback;
+        try
+        {
+            while (!Cts.Token.IsCancellationRequested &&
+                   await DownloadQueue.Reader.WaitToReadAsync(Cts.Token))
+            {
+                // anonymous
+                if (ContentDownloader.InitializeSteam3(null, null))
+                {
+                    try
+                    {
+                        while (DownloadQueue.Reader.TryRead(out var entry))
+                        {
+                            try
+                            {
+                                CurrentDownloadEntry = entry;
+                                // Do the true download operations
+                                ContentDownloader.Config.InstallDirectorySuffix = entry.PubFileId.ToString();
+                                await ContentDownloader.DownloadPubfileAsync(APP_ID, entry.PubFileId);
+                                ContentDownloader.Config.InstallDirectorySuffix = string.Empty;
+                            }
+                            catch (Exception ex) when (ex is not OperationCanceledException)
+                            {
+                                await ExceptionQueue.Writer.WriteAsync((entry, ex));
+                            }
+                            finally
+                            {
+                                CurrentDownloadEntry = null;
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        ContentDownloader.ShutdownSteam3();
+                    }
+                }
+                else
+                {
+                    await ExceptionQueue.Writer.WriteAsync((null, new Exception("Failed to login as anonymous")));
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // no op
+        }
+        catch (Exception ex)
+        {
+            await ExceptionQueue.Writer.WriteAsync((null, ex));
+        }
+        finally
+        {
+            ContentDownloader.MessageCallback += msgCallback;
+            ContentDownloader.Config.InstallDirectorySuffix = string.Empty;
+        }
+    }
+}
